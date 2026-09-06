f83826f65d03caa6d0d28cd050b16d72a468748a
Luan Silva <151473822+Avlis170@users.noreply.github.com>
2026-09-05 23:34:00 -0300

    Update and rename bibliotech.py. to bibliotech.py

---
+++ src/bibliotech.py
@@ -0,0 +1,28 @@
+LIMITE_EMPRESTIMOS = 3
+
+def pode_emprestar(usuario_ativo, possui_pendencia, emprestimos_ativos):
+    if not usuario_ativo:
+        return False
+    if possui_pendencia:
+        return False
+    if emprestimos_ativos > LIMITE_EMPRESTIMOS:
+        return False
+    return True
+
+def calcular_multa(dias_atraso):
+    if dias_atraso <= 0:
+        return 0.0
+    elif dias_atraso <= 7:
+        return dias_atraso * 2.0
+    else:
+        return 14.0 + (dias_atraso - 7) * 3.0
+
+def classificar_atraso(dias_atraso):
+    if dias_atraso <= 0:
+        return "sem atraso"
+    elif dias_atraso <= 7:
+        return "atraso leve"
+    elif dias_atraso <= 30:
+        return "atraso moderado"
+    else:
+        return "atraso grave"
